79b4f7cacfa2ff2802783fe120a75851ece1d206
Desklay Amrita <amrita.dubey@desklay.com>
2023-07-10 19:16:45 +0530

    update resume section

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,27 @@ document.addEventListener('keydown', (e) => {
         e.preventDefault();
     }
 });
+
+
+
+$(document).ready(function() {
+    $('#downloadBtn').click(function() {
+      // Replace 'path/to/your/pdf.pdf' with the actual path to your PDF file
+      var pdfUrl = 'path/to/your/pdf.pdf';
+      
+      // Create a hidden <a> element with the PDF URL
+      var link = document.createElement('a');
+      link.href = pdfUrl;
+      link.download = 'downloaded_pdf.pdf'; // Specify the name for the downloaded file
+      
+      // Append the <a> element to the document
+      document.body.appendChild(link);
+      
+      // Programmatically trigger a click event on the <a> element
+      link.click();
+      
+      // Remove the <a> element from the document
+      document.body.removeChild(link);
+    });
+  });
+  
